a25f6c4db30bcf2eb9168b97d9c633c8eaba56fc
Paras Bhatiwal <67578632+parasbhatiwal@users.noreply.github.com>
2021-04-04 11:49:03 +0530

    removed alert

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ document.getElementById('btn').addEventListener('click', () => {
     if (nameid === '') {
         alert('Please input Your Name')
     } else {
-
-        alert(nameid)
         document.getElementById('imgchange').src = `//joeschmoe.io/api/v1/${nameid} `
     }
 })
